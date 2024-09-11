4abf018c37f99644fdc16e7acb8acfd5d83dd9d2
swainealex <160972944+swainealex@users.noreply.github.com>
2024-09-11 18:49:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -282,9 +282,9 @@ def update_all_visualizations(n, n_refresh):
         status_message
     ]
 
-server = app.server
+
 app = Dash(__name__)
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=8000, debug=True)
-
+server = app.server
